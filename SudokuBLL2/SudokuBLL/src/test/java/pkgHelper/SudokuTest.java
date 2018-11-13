82dd307b409b7bffeb58a752d8e6f422995ed4c8
brenn <brenn@192.168.0.221>
2018-11-13 00:17:22 -0500

    lab 5 w test

--- SudokuBLL2/SudokuBLL/src/test/java/pkgHelper/SudokuTest.java
+++ SudokuBLL2/SudokuBLL/src/test/java/pkgHelper/SudokuTest.java
@@ -1,5 +1,6 @@
 package pkgHelper;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.junit.Test;
@@ -17,9 +18,38 @@ public class SudokuTest {
 		
 	}
 	
-	@Test
-	public void PossibleValuesMultiplierTest() throws Exception {
-		
-	}
+//	@Test
+//	public void PossibleValuesMultiplierTest() throws Exception {
+//		Sudoku s = new Sudoku(9);
+//		HashMap<Integer,Sudoku.SudokuCell> cells = new HashMap<Integer,Sudoku.SudokuCell>();
+//		System.out.println(PossibleValuesMultiplier(cells));
+//	}
+//
+//	private int PossibleValuesMultiplier(HashMap<Integer, SudokuCell> cells) {
+//int multiplier = 1;
+//		
+//		for (SudokuCell i : cells.values()) {
+//			multiplier *= i.getLstValidValues().size();
+//			if (multiplier > Integer.MAX_VALUE) {
+//				return Integer.MAX_VALUE;
+//			}
+//			else {
+//				multiplier *= i.getLstValidValues().size();
+//			}
+//		}
+//		return multiplier;
+//	}
+	
+//@Test
+//public void isDifficultyMetTest() throws Exception {
+//	Sudoku s = new Sudoku(9, eGameDifficulty.MEDIUM);
+//	System.out.println(s.isDifficultyMet(100));
+//}
+
+//	@Test
+//	public void RemoveCellsTest() throws Exception {
+//		Sudoku s = new Sudoku(9);
+//		s.RemoveCells();
+//	}
 
 }
